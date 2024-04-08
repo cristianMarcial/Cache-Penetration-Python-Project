8fb155083bed4341f0591fd74eb352e354916b78
cristianMacial <cristian.marcial@upr.edu>
2024-04-08 18:25:08 -0400

    Reduced as possible version of micro

--- main.py
+++ main.py
@@ -1,35 +1,13 @@
 from sys import argv; from math import ceil, log; import csv
-
-# This function returns an integer resulting from the value of the "entry" parameter passed by the function "hash" k times.
-def hashing(entry):
-    for i in range(k): entry = hash(entry) % m
-    return entry
-
-if len(argv) > 1:
-    # The first file opening consist in counting the lines in the first input file and storing the value in 'n' variable
-    with open(argv[1]) as file: 
-        file.readline() # This omits the first row on the file
-
-        # Number of items in the bloom filter.
-        n = sum(1 for row in csv.reader(file)) 
-
-        # Number of bits in the filter.
-        m = ceil((n * log(0.0000001)) / log(1 / 2**log(2))) 
-
-        # Number of hash functions.
-        k = round((m / n) * log(2)) 
-
-        bloomFilter = [False for i in range(m)]
-
-        # The first input file is read from the beginning again in order to give the bloom filter values.
-        file.seek(1)
-        for line in csv.reader(file): 
-            bloomFilter[hashing(str(line))] = True
-    file.close()
-
-    # Finally, the second input file is opened to test its entries against the bloom filter.
-    with open(argv[2]) as file: 
-        file.readline()
-        for line in csv.reader(file): 
-            print(line[0] + ',' + ("Probably in the DB" if (bloomFilter[hashing(str(line))] == True) else "Not in the DB"))
-    file.close()
+with open(argv[1]) as file: 
+    file.readline()
+    n = sum(1 for row in csv.reader(file)) 
+    m = ceil((n * log(0.0000001)) / log(1 / 2**log(2))) 
+    bF = [False for i in range(m)]
+    file.seek(1)
+    for line in csv.reader(file): bF[hash(str(line)) % m] = True
+file.close()
+with open(argv[2]) as file:
+    file.readline()
+    for l in csv.reader(file): print(l[0] + ',' + ("Probably in the DB" if (bF[hash(str(l)) % m] == True) else "Not in the DB"))
+file.close()
